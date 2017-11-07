0722cd740360f46d1985fe1797b858d78b4bcb2a
Coviam <coviam@Coviams-MacBook-Pro.local>
2017-11-07 15:00:08 +0530

    jslint error fixed

--- balanced_parentheses.js
+++ balanced_parentheses.js
@@ -1,24 +1,24 @@
-function balanceParenthesis(str){
+function balanceParenthesis(str) {
   var stack = [];
-  for(var i = 0; i<str.length; i++){
+  for (var i = 0; i < str.length; i++) {
     var l = stack.length;
-    if(str[i] === '(' || str[i] === '[' || str[i] === '{'){
+    if (str[i] === '(' || str[i] === '[' || str[i] === '{') {
       stack.push(str[i]);
-    } 
-    else if(l >=1 && str[i] === ')'&& stack[l-1] === '('){
+    }
+    else if (l >= 1 && str[i] === ')' && stack[l - 1] === '(') {
       stack.pop();
     }
-    else if(l >=1 && str[i] === ']'&& stack[l-1] === '['){
+    else if (l >= 1 && str[i] === ']' && stack[l - 1] === '[') {
       stack.pop();
     }
-    else if(l >=1 && str[i] === '}'&& stack[l-1] === '{'){
+    else if (l >= 1 && str[i] === '}' && stack[l - 1] === '{') {
       stack.pop();
-    } 
+    }
     else {
       return false;
     }
   }
-  if(stack.length === 0){
+  if (stack.length === 0) {
     return true
   }
   else {
